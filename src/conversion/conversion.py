b8801bfe2e42eaf82cec31d6cd6198558fe3d1cf
David Valderrama <davidvg07049@gmail.com>
2025-08-26 21:56:38 -0500

    Convertir numero decimal a numero romano

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -8,46 +8,50 @@ class Conversion:
     
     def metros_a_pies(self, metros):
         """
-        Convierte distancia de metros a pies.
-        
+        Convierte distancia de metros a pies.   
         """
         return metros * 3.28084
     
     def pies_a_metros(self, pies):
         """
-        Convierte distancia de pies a metros.
-        
+        Convierte distancia de pies a metros.   
         """
         return pies * 0.3048
     
     def decimal_a_binario(self, decimal):
         """
-        Convierte un número decimal a su representación binaria.
-        
+        Convierte un número decimal a su representación binaria.  
         """
         return bin(decimal)[2:]
     
     def binario_a_decimal(self, binario):
         """
-       
+       Convierte un número binario a decimal.
         """
         return int(binario, 2)
     
     def decimal_a_romano(self, numero):
         """
         Convierte un número decimal a numeración romana.
-        
-        Args:
-            numero (int): Número decimal entre 1 y 3999
-            
-        Returns:
-            str: Número romano
-            
-        Ejemplo:
-            decimal_a_romano(9) -> "IX"
-            decimal_a_romano(1994) -> "MCMXCIV"
         """
-        pass
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+        romano = ""
+        i = 0
+        while numero > 0:
+            for _ in range(numero // val[i]):
+                romano += syms[i]
+                numero -= val[i]
+            i += 1
+        return romano
     
     def romano_a_decimal(self, romano):
         """
