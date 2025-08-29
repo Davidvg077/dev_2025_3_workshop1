d9835240821e38b3a6907699c873875d0764abe4
David Valderrama <davidvg07049@gmail.com>
2025-08-28 20:49:29 -0500

    area del pentago

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -66,15 +66,9 @@ class Geometria:
     def area_pentagono_regular(self, lado, apotema):
         """
         Calcula el área de un pentágono regular.
-        
-        Args:
-            lado (float): Longitud del lado del pentágono
-            apotema (float): Longitud de la apotema (distancia del centro al punto medio de un lado)
-            
-        Returns:
-            float: Área del pentágono regular
         """
-        pass
+        perimetro = 5 * lado
+        return (perimetro * apotema) / 2    
     
     def perimetro_pentagono_regular(self, lado):
         """
