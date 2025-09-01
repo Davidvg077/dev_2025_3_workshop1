3e766150d71fb92aadf6a653f0e4d4ba27e72ddf
David Valderrama <davidvg07049@gmail.com>
2025-09-01 10:41:43 -0500

    implementacion logica not

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -19,14 +19,8 @@ class Logica:
     def NOT(self, a):
         """
         Implementa la operación lógica NOT.
-        
-        Args:
-            a (bool): Valor booleano
-            
-        Returns:
-            bool: Resultado de NOT a
         """
-        pass
+        return not a
     
     def XOR(self, a, b):
         """
